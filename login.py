f745f4bf343a93b03c4a5b06c74addb25185ca7e
Krii101 <121224520+Krii101@users.noreply.github.com>
2023-02-04 19:01:50 +0545

    Add files via upload

---
+++ login.py
@@ -0,0 +1,124 @@
+from tkinter import *
+from PIL import Image, ImageTk
+from tkinter.font import Font
+#4A78A9
+white="#CBD8ED"
+bgblue="#4A78A9"
+dblue="#4A78A9"
+#button commands---------------------------------------
+def registration():
+       loginp.destroy()
+       import register
+
+loginp=Tk()
+loginp.minsize(1000,655)
+loginp.maxsize(1000,655)
+loginp.title("CV-ANCIES")
+loginp.iconbitmap('logo..ico')
+loginp.config(bg="#4A78A9")
+#fonst------------------------------------------------
+my_font0 = Font(
+    family = 'Lucida sans',
+    size = 11,
+    weight='bold',
+    slant = 'roman',
+    overstrike = 0)
+my_font = Font(
+    family = 'Lucida sans',
+    size = 13,
+    weight='bold',
+    slant = 'roman',
+    overstrike = 0)
+my_font1 = Font(
+        family = 'Lucida sans',
+        size = 16,
+        weight='bold',
+        slant = 'roman',
+        overstrike = 0)
+my_font2 = Font(
+        family = 'Lucida sans',
+        size = 28,
+        weight='bold',
+        slant = 'roman',
+        overstrike = 0)
+
+#logo------------------------------------------
+img=Image.open('lg.png')
+img1=img.resize((400,320))
+logo=ImageTk.PhotoImage(img1)
+logo_p=Label(loginp,image=logo,bg="#4A78A9")
+logo_p.place(x=550,y=150)
+
+#login frame------------------------------------
+log=Frame(loginp,bg="white",borderwidth=1)
+log.place(x=150,y=135)
+log_ttle=Label(log,text="LOGIN",fg="#CBD8ED",bg="#CBD8ED",font=my_font2,padx=100,pady=165)
+log_ttle.grid()
+
+#event-entries--------------------------------
+def del1(event):
+       user_entry.delete(0,END)
+def del2(e):
+       pw_entry.delete(0,END)
+       
+#labels and entry---------------------------------------------------------------
+login_lb=Label(log,text="LOGIN",font=my_font2,bg="#CBD8ED",fg="black")
+login_lb.place(x=100,y=35)
+
+user=Label(log,text="Username:",font=my_font1,bg="#CBD8ED",fg="black")
+user.place(y=98,x=25)
+
+user_entry=Entry(log,bg="#CBD8ED",font=my_font1,fg="#2B2828",width=20,borderwidth=2,relief=GROOVE)
+user_entry.insert(0,"Username")
+user_entry.place(x=27,y=130)
+user_entry.bind("<FocusIn>",del1)
+
+#passworddd----------------------------------------
+pw=Label(log,text="Password:",font=my_font1,bg="#CBD8ED",fg="black")
+pw.place(y=175,x=25)
+
+pw_entry=Entry(log,bg="#CBD8ED",font=my_font1,fg="#2B2828",width=20,borderwidth=2,relief=GROOVE,show="*")
+pw_entry.insert(0,"Password")
+pw_entry.place(x=27,y=205)
+pw_entry.bind("<FocusIn>",del2)
+
+a=Image.open('show.png')
+a1=a.resize((20,20))
+show_img=ImageTk.PhotoImage(a1)
+
+b=Image.open('hide.png')
+b1=b.resize((20,20))
+hide_img=ImageTk.PhotoImage(b1)
+
+    #hide/show functions 
+def hide():
+       show_btn=Button(log,image=show_img,command=show,bg="#CBD8ED",borderwidth=0,activebackground="#CBD8ED")
+       show_btn.place(y=208,x=265)
+       pw_entry.config(show="*")
+
+def show():
+       hide_btn=Button(log,image=hide_img,command=hide,bg="#CBD8ED",borderwidth=0,activebackground="#CBD8ED")
+       hide_btn.place(y=208,x=265)
+       pw_entry.config(show="")
+
+show_btn=Button(log,image=show_img,command=show,bg="#CBD8ED",borderwidth=0,activebackground="#CBD8ED")
+show_btn.place(y=208,x=265)
+hide_btn=Button(log,image=hide_img,command=hide,bg="#CBD8ED",borderwidth=0,activebackground="#CBD8ED")
+hide_btn.place(y=208,x=265)
+
+#loging button---------------------------------------------
+log_btn=Button(log,bg="#3D5A80",fg='white',text="LOGIN",padx=95,font=my_font)
+log_btn.place(x=27,y=255)
+
+new=Label(log,bg="#CBD8ED",text="New to vancies?",fg="#2B2828",font=my_font0)
+new.place(x=94,y=300)
+
+register_btn=Button(log,bg="#4A78A9",fg='white',text="REGISTER",font=my_font0,command=registration)
+register_btn.place(x=110,y=329)
+
+img2=Image.open('help.png')
+img3=img2.resize((20,20))
+help_ico=ImageTk.PhotoImage(img3)
+help_btn=Button(loginp,image=help_ico,bg="#fCA311").place(x=1050,y=2)
+
+loginp.mainloop()
