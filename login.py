af5d459ee32c6bc14a9ed8dad4ed87a621a97955
Sachi <118369258+kiyo-9@users.noreply.github.com>
2023-02-13 21:30:06 +0545

    Update login.py

--- login.py
+++ login.py
@@ -4,9 +4,9 @@ from tkinter.font import Font
 from tkinter import messagebox
 import sqlite3
 #4A78A9
-white="#CBD8ED"
-bgblue="#4A78A9"
-dblue="#4A78A9"
+# white="#CBD8ED"
+# bgblue="#4A78A9"
+# dblue="#4A78A9"
 #button commands---------------------------------------
 def registration():
        loginp.destroy()
@@ -26,12 +26,12 @@ def check():
     a=user_entry.get()
     b=pw_entry.get()
     try:
-        conn=sqlite3.connect("Check.db")
-        c=conn.cursor()
+        log=sqlite3.connect("Check.db")
+        log1=log.cursor()
 
-        c.execute("SELECT * from vancy")
+        log1.execute("SELECT * from vancy")
         global records
-        records=c.fetchall()
+        records=log1.fetchall()
         global i
         i=len(records)-1
         while i>=0:
@@ -40,12 +40,13 @@ def check():
                 if i==-1:
                     messagebox.showerror("Login","Invalid Credentials")
                     break
-                conn.commit()
+            else:    
+                log.commit()
                 messagebox.showinfo("Login","Logged in Successfully")
-                # categ()
+                categ()
                 break           
-        conn.commit()
-        conn.close()
+        log.commit()
+        log.close()
     except:
         messagebox.showerror("Login","Sign Up First")
 
@@ -93,9 +94,9 @@ logo_p=Label(loginp,image=logo,bg="#4A78A9")
 logo_p.place(x=550,y=150)
 
 #login frame------------------------------------
-log=Frame(loginp,bg="white",borderwidth=1)
-log.place(x=150,y=135)
-log_ttle=Label(log,text="LOGIN",fg="#CBD8ED",bg="#CBD8ED",font=my_font2,padx=100,pady=165)
+logon=Frame(loginp,bg="white",borderwidth=1)
+logon.place(x=150,y=135)
+log_ttle=Label(logon,text="LOGIN",fg="#CBD8ED",bg="#CBD8ED",font=my_font2,padx=100,pady=165)
 log_ttle.grid()
 
 #event-entries--------------------------------
@@ -109,49 +110,25 @@ def del2(e):
        pw_entry.delete(0,END)
        
 #labels and entry---------------------------------------------------------------
-login_lb=Label(log,text="LOGIN",font=my_font2,bg="#CBD8ED",fg="black")
+login_lb=Label(logon,text="LOGIN",font=my_font2,bg="#CBD8ED",fg="black")
 login_lb.place(x=100,y=35)
 
-user=Label(log,text="Username:",font=my_font1,bg="#CBD8ED",fg="black")
+user=Label(logon,text="Username:",font=my_font1,bg="#CBD8ED",fg="black")
 user.place(y=98,x=25)
 
-user_entry=Entry(log,bg="#CBD8ED",font=my_font1,fg="#2B2828",width=20,borderwidth=2,relief=GROOVE)
+user_entry=Entry(logon,bg="#CBD8ED",font=my_font1,fg="#2B2828",width=20,borderwidth=2,relief=GROOVE)
 user_entry.insert(0,"Username")
 user_entry.place(x=27,y=130)
 user_entry.bind("<FocusIn>",del1)
 
 #passworddd----------------------------------------
-pw=Label(log,text="Password:",font=my_font1,bg="#CBD8ED",fg="black")
+pw=Label(logon,text="Password:",font=my_font1,bg="#CBD8ED",fg="black")
 pw.place(y=175,x=25)
 
-pw_entry=Entry(log,bg="#CBD8ED",font=my_font1,fg="#2B2828",width=20,borderwidth=2,relief=GROOVE,show="*")
+pw_entry=Entry(logon,bg="#CBD8ED",font=my_font1,fg="#2B2828",width=20,borderwidth=2,relief=GROOVE,show="*")
 pw_entry.insert(0,"Password")
 pw_entry.place(x=27,y=205)
 pw_entry.bind("<FocusIn>",del2)
-
-a=Image.open('show.png')
-a1=a.resize((20,20))
-show_img=ImageTk.PhotoImage(a1)
-
-b=Image.open('hide.png')
-b1=b.resize((20,20))
-hide_img=ImageTk.PhotoImage(b1)
-
-    #hide/show functions 
-def hide():
-       show_btn=Button(log,image=show_img,command=show,bg="#CBD8ED",borderwidth=0,activebackground="#CBD8ED")
-       show_btn.place(y=208,x=245)
-       pw_entry.config(show="")
-
-def show():
-       hide_btn=Button(log,image=hide_img,command=hide,bg="#CBD8ED",borderwidth=0,activebackground="#CBD8ED")
-       hide_btn.place(y=208,x=245)
-       pw_entry.config(show="*")
-
-show_btn=Button(log,image=show_img,command=show,bg="#CBD8ED",borderwidth=0,activebackground="#CBD8ED")
-show_btn.place(y=208,x=245)
-hide_btn=Button(log,image=hide_img,command=hide,bg="#CBD8ED",borderwidth=0,activebackground="#CBD8ED")
-hide_btn.place(y=208,x=245)
 
 #verify function-------------------------------------
 def verify():
@@ -163,17 +140,42 @@ def verify():
         messagebox.showerror("Password Reset","Password must be more than 6 characters")
     else:
         check()  
+
+a=Image.open('show.png')
+a1=a.resize((20,20))
+show_img=ImageTk.PhotoImage(a1)
+
+b=Image.open('hide.png')
+b1=b.resize((20,20))
+hide_img=ImageTk.PhotoImage(b1)
             
 
 
+    #hide/show functions 
+def hide():
+       show_btn=Button(logon,image=show_img,command=show,bg="#CBD8ED",borderwidth=0,activebackground="#CBD8ED")
+       show_btn.place(y=208,x=245)
+       pw_entry.config(show="")
+
+def show():
+       hide_btn=Button(logon,image=hide_img,command=hide,bg="#CBD8ED",borderwidth=0,activebackground="#CBD8ED")
+       hide_btn.place(y=208,x=245)
+       pw_entry.config(show="*")
+
+show_btn=Button(logon,image=show_img,command=show,bg="#CBD8ED",borderwidth=0,activebackground="#CBD8ED")
+show_btn.place(y=208,x=245)
+hide_btn=Button(logon,image=hide_img,command=hide,bg="#CBD8ED",borderwidth=0,activebackground="#CBD8ED")
+hide_btn.place(y=208,x=245)
+
+
 #loging button---------------------------------------------
-log_btn=Button(log,bg="#3D5A80",fg='white',text="LOGIN",padx=95,font=my_font,command=verify)
+log_btn=Button(logon,bg="#3D5A80",fg='white',text="LOGIN",padx=95,font=my_font,command=verify)
 log_btn.place(x=27,y=255)
 
-new=Label(log,bg="#CBD8ED",text="New to vancies?",fg="#2B2828",font=my_font0)
+new=Label(logon,bg="#CBD8ED",text="New to vancies?",fg="#2B2828",font=my_font0)
 new.place(x=94,y=300)
 
-register_btn=Button(log,bg="#4A78A9",fg='white',text="REGISTER",font=my_font0,command=registration)
+register_btn=Button(logon,bg="#4A78A9",fg='white',text="REGISTER",font=my_font0,command=registration)
 register_btn.place(x=110,y=329)
 
 
@@ -184,6 +186,7 @@ help_ico=ImageTk.PhotoImage(img3)
 help_btn=Button(loginp,image=help_ico,bg="#fCA311").place(x=1050,y=2)
 
 loginp.mainloop()
+
 
 
 
